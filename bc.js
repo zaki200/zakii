4d046203fc70c751bf55203b511fd97d2ac8355c
zaki200 <51976654+zaki200@users.noreply.github.com>
2019-06-19 03:44:11 +0200

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,32 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "!z";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Sanchiro l سنشيرو`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login("NTkwNzEyMzkzMDM4OTU0NTAz.XQmRgA.cCD0IWzfVwoif0OsS47WlAVhV-8");
